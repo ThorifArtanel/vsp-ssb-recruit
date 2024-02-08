4bf7828cff32bef9d8d52ca522efcfcefe15e3f4
ThorifArtanel <thorif.m.artanel@gmail.com>
2024-02-08 15:25:10 +0700

    refactor: change html file parsing

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	api "gitlab.com/vsp-ssb/landing-page-recruitment/api"
 )
@@ -13,13 +12,20 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.StaticFS("/assets/images", http.Dir("./web/static/images"))
-	r.StaticFS("/assets/svg", http.Dir("./web/static/svg"))
-	r.StaticFS("/assets/css", http.Dir("./web/static/css"))
-	r.StaticFS("/assets/js", http.Dir("./web/static/js"))
-	r.StaticFS("/assets/plugins", http.Dir("./web/static/plugins"))
+	r.StaticFS("/assets/images", http.Dir("web/static/images"))
+	r.StaticFS("/assets/svg", http.Dir("web/static/svg"))
+	r.StaticFS("/assets/css", http.Dir("web/static/css"))
+	r.StaticFS("/assets/js", http.Dir("web/static/js"))
+	r.StaticFS("/assets/plugins", http.Dir("web/static/plugins"))
 
-	r.Use(static.Serve("/", static.LocalFile("./web/static/html", true)))
+	r.LoadHTMLGlob("web/static/html/*.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusNotFound, "404.html", gin.H{})
+	})
 
 	r.POST("/api/contact-form", api.FormHandler)
 
